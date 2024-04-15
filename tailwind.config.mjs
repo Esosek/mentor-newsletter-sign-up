6a5755f72cedde2b10fdc421ab58fc688d0c68cf
Ales Zima <ales@cpex.cz>
2024-04-15 15:00:55 +0200

    Added color theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'hsl(4, 100%, 67%)',
+        'grey-dark': 'hsl(235, 18%, 26%)',
+        grey: 'hsl(231, 7%, 60%)',
+      },
+    },
+  },
+  plugins: [],
+};
